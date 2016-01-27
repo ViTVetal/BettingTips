f81b28083ccb2b572153f59d6616b786ad8ef06b
ViTVetal <vvk1992@ukr.net>
2016-01-27 21:20:16 +0200

    add gcm tokens to server

--- backend/app/controllers/api/gcms_controller.rb
+++ backend/app/controllers/api/gcms_controller.rb
@@ -2,6 +2,7 @@ class Api::GcmsController < ApplicationController
 	skip_before_filter  :verify_authenticity_token
   def create
   	Gcm.create(gcm_params)
+  	render :nothing => true
   end
 
   def gcm_params
